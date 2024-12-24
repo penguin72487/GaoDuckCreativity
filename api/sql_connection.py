b73d31b501ac4704e79f6e88c31e66d646586f37
Jaxx9527 <envy-crease-thee@duck.com>
2024-12-24 22:46:45 +0800

    新增使用者修改密碼函式&demo用法

--- api/sql_connection.py
+++ api/sql_connection.py
@@ -131,6 +131,39 @@ class SqlAPI:
             self.connection.rollback()
             return f"註冊失敗: {e}"
 
+    def userchangepassword(self, u_id, old_password, new_password):
+        """
+        使用者已知密碼情況下改密碼。
+        :param u_id: user id
+        :param old_password: 原密碼
+        :param new_password: 新密碼
+        :return: 成功if成功 or 失敗if老密碼錯誤
+
+        """
+        # 檢查是否存在該使用者且舊密碼正確
+        base_query_of_check= """
+        SELECT COUNT(*) FROM `user`
+        WHERE u_id = %s AND password = %s
+        """
+        self.cursor.execute(base_query_of_check, (u_id, old_password))
+        result = self.cursor.fetchone()
+
+        if result[0] == 0:
+            return "原密碼錯誤"
+
+        # 更新密碼
+        base_query_of_update = """
+        UPDATE `user`
+        SET password = %s 
+        WHERE u_id = %s
+        """
+        self.cursor.execute(base_query_of_update, (new_password, u_id))
+        self.connection.commit()
+
+        return "密碼修改成功"
+
+
+
     def close_connection(self):
         """
         關閉資料庫連線。
@@ -149,60 +182,62 @@ if __name__ == "__main__":
 
 
 
-# 中華民國身分證產生器
-# https://people.debian.org/~paulliu/ROCid.html
+    # 中華民國身分證產生器
+    # https://people.debian.org/~paulliu/ROCid.html
 
     db = SqlAPI()
+    print(db.userchangepassword(18,"securepassword","saltedpassword7777"))
+    print(db.userchangepassword(10, "worng_old_pw", "saltedpassword7777"))
 
-    # 用戶資訊
-    result = db.userreg(
-        id_num="A147909161",
-        name="學生1",
-        phone="0922334455",
-        email="student@example.com",
-        password="securepassword",
-        address="新北市板橋區",
-        user_type="student",
-        s_t_id=None             # None is Null in sql
-    )
-
-
-    print(result)  # 輸出註冊結果
-
-    result = db.userreg(
-        id_num="A102954995",
-        name="評分者1",
-        phone="0912345678",
-        email="rater@example.com",
-        password="securepassword",
-        address="台北市大安區",
-        user_type="rater",
-        rater_title="筑波大學電腦科學系副教授"
-    )
-    print(result)
-
-    result = db.userreg(
-        id_num="C117528249",
-        name="教師1",
-        phone="0933445566",
-        email="teacher@example.com",
-        password="securepassword",
-        address="高雄市苓雅區",
-        user_type="teacher",
-        t_t_id=None  # 所屬團隊 ID
-    )
-    print(result)
-    result = db.userreg(
-        id_num="C114437590",
-        name="超級admin",
-        phone="0987654321",
-        email="admin@example.com",
-        password="securepassword",
-        address="國立高雄大學創新學院",
-        user_type="admin",
-        admin_type=999
-    )
-    print(result)
+    #    # 用戶資訊
+    #    result = db.userreg(
+    #        id_num="A147909161",
+    #        name="學生1",
+    #        phone="0922334455",
+    #        email="student@example.com",
+    #        password="securepassword",
+    #        address="新北市板橋區",
+    #        user_type="student",
+    #        s_t_id=None             # None is Null in sql
+    #    )
+    #
+    #
+    #    print(result)  # 輸出註冊結果
+    #
+    #    result = db.userreg(
+    #        id_num="A102954995",
+    #        name="評分者1",
+    #        phone="0912345678",
+    #        email="rater@example.com",
+    #        password="securepassword",
+    #        address="台北市大安區",
+    #        user_type="rater",
+    #        rater_title="筑波大學電腦科學系副教授"
+    #    )
+    #    print(result)
+    #
+    #    result = db.userreg(
+    #        id_num="C117528249",
+    #        name="教師1",
+    #        phone="0933445566",
+    #        email="teacher@example.com",
+    #        password="securepassword",
+    #        address="高雄市苓雅區",
+    #        user_type="teacher",
+    #        t_t_id=None  # 所屬團隊 ID
+    #    )
+    #    print(result)
+    #    result = db.userreg(
+    #        id_num="C114437590",
+    #        name="超級admin",
+    #        phone="0987654321",
+    #        email="admin@example.com",
+    #        password="securepassword",
+    #        address="國立高雄大學創新學院",
+    #        user_type="admin",
+    #        admin_type=999
+    #    )
+    #    print(result)
 
     # 關閉資料庫連線
     db.close_connection()

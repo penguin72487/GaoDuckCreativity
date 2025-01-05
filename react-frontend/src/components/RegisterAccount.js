10912b918628704ff9fa36fdc794f1827dcd42f5
SiroKu_ <56777898+SiroKu1006@users.noreply.github.com>
2025-01-05 22:53:51 +0800

    移除註冊表單中的角色選項文字，改為使用數字代碼表示帳號類別

--- react-frontend/src/components/RegisterAccount.js
+++ react-frontend/src/components/RegisterAccount.js
@@ -61,10 +61,6 @@ const RegisterAccount = () => {
 
                 <label>帳號類別</label>
                 <select name="role" onChange={handleChange}>
-                    <option value="student">學生</option>
-                    <option value="teacher">老師</option>
-                    <option value="judge">評審</option>
-                    <option value="admin">管理者</option>
                     <option value="1">學生</option>
                     <option value="2">老師</option>
                     <option value="3">評委</option>
